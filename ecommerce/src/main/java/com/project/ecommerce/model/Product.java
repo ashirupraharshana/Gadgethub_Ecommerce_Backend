97b39126995013750ffdd7159be78ffb476c33c7
ashirupraharshana <ashirupraharshana@gmail.com>
2025-11-06 10:00:55 +0530

    feat: extend Product model with quantity and ratings, update API tests

--- ecommerce/src/main/java/com/project/ecommerce/model/Product.java
+++ ecommerce/src/main/java/com/project/ecommerce/model/Product.java
@@ -2,12 +2,11 @@ package com.project.ecommerce.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.util.Date;
 
 @Data
 @Entity
-@Table(name = "products")
+@Table(name="products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,9 +14,12 @@ public class Product {
 
     private int categoryid;
     private String name;
-    private String description;
+    private String descreption;
     private String photo;
     private int status;
     private int discount;
+    private double price;
+    private int quantity;
+    private int ratings;
     private Date date;
 }
