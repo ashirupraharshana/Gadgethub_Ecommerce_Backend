e357c4ba024944a69c047061d389f020c9387b46
ashirupraharshana <ashirupraharshana@gmail.com>
2025-11-06 13:10:47 +0530

    feat: implement user login and registration with password encryption

--- ecommerce/src/main/java/com/project/ecommerce/controller/UserController.java
+++ ecommerce/src/main/java/com/project/ecommerce/controller/UserController.java
@@ -3,30 +3,63 @@ package com.project.ecommerce.controller;
 import com.project.ecommerce.model.User;
 import com.project.ecommerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    // Register a new user
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    //  Register a new user
     @PostMapping("/register")
-    public User registerUser(@RequestBody User user) {
-        return userService.registerUser(user);
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        if (userService.findByEmail(user.getEmail()) != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(Map.of("message", "Email already exists"));
+        }
+        User savedUser = userService.registerUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(Map.of("message", "User registered successfully", "user", savedUser));
     }
 
-    // Get all users
+    //  Login user
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody Map<String, String> loginData) {
+        String email = loginData.get("email");
+        String password = loginData.get("password");
+
+        User user = userService.findByEmail(email);
+
+        if (user == null || !passwordEncoder.matches(password, user.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", "Invalid email or password"));
+        }
+
+        return ResponseEntity.ok(Map.of(
+                "message", "Login successful",
+                "user", user
+        ));
+    }
+
+    //  Get all users
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    // Find user by email
+    //  Find user by email
     @GetMapping("/{email}")
     public User getUserByEmail(@PathVariable String email) {
         return userService.findByEmail(email);
